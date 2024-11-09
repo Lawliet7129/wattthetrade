44650b92c4f4b87364045c3603527f9a7a986b42
Bailey Koo <bkoo27@seas.upenn.edu>
2024-11-09 00:11:10 -0500

    Initial commit for energy trading platform

--- frontend/public/script.js
+++
@@ -1,8 +0,0 @@
-async function fetchOptionPrices() {
-    const response = await fetch('/option-prices');
-    const data = await response.json();
-    document.getElementById('optionPrices').innerText = 
-        `Call Price: $${data.callPrice}, Put Price: $${data.putPrice}`;
-}
-
-setInterval(fetchOptionPrices, 5000); // Update every 5 seconds
--- matlab_scripts/BlackScholes.m
+++ matlab_scripts/BlackScholes.m
@@ -1,14 +1,11 @@
 % BlackScholes.m
 function [callPrice, putPrice] = BlackScholes(S, K, T, r, sigma)
-    % Calculate d1 and d2
-    d1 = (log(S / K) + (r + sigma^2 / 2) * T) / (sigma * sqrt(T));
+    d1 = (log(S / K) + (r + 0.5 * sigma^2) * T) / (sigma * sqrt(T));
     d2 = d1 - sigma * sqrt(T);
 
-    % Calculate call and put prices
     callPrice = S * normcdf(d1) - K * exp(-r * T) * normcdf(d2);
     putPrice = K * exp(-r * T) * normcdf(-d2) - S * normcdf(-d1);
 
-    % Save results to JSON for use in backend
     optionPrices = struct('callPrice', callPrice, 'putPrice', putPrice);
     savejson('', optionPrices, 'optionPrices.json');
 end
