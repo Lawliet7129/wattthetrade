b9767ff67e5af21a5049db022ffbe7c2f8ee4aae
Bailey Koo <bkoo27@seas.upenn.edu>
2024-11-09 00:45:32 -0500

    Add/update wsgi.py with correct permissions and DJANGO_SETTINGS_MODULE

--- backend/energy_trading/energy_trading/wsgi.py
+++ backend/energy_trading/energy_trading/wsgi.py
@@ -1,5 +1,5 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'energy_trading.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'energy_trading.energy_trading.settings')
 application = get_wsgi_application()
