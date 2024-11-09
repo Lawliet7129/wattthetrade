ed1d1c3f1e6cd532604f730a09ccedf82023f861
Bailey Koo <bkoo27@seas.upenn.edu>
2024-11-09 00:48:00 -0500

    Fix DJANGO_SETTINGS_MODULE path

--- backend/energy_trading/energy_trading/wsgi.py
+++ backend/energy_trading/energy_trading/wsgi.py
@@ -2,4 +2,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'energy_trading.energy_trading.settings')
+
 application = get_wsgi_application()
---
+++ backend/manage.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'energy_trading.energy_trading.settings')
+
+
+
+if __name__ == '__main__':
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
