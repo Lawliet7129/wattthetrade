da1199ab27467c73b667aab958982335d5b09c18
Bailey Koo <bkoo27@seas.upenn.edu>
2024-11-09 00:51:48 -0500

    Update settings and ensure all necessary files are committed

--- backend/energy_trading/asgi.py
+++ backend/energy_trading/asgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "energy_trading.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'energy_trading.energy_trading.settings')
+
 
 application = get_asgi_application()
